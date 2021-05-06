a91a85aa957338ff5d2bb41b7fe93f01a183c2aa
Luís Freitas <microchipgnu@gmail.com>
2021-05-06 16:40:59 +0100

    hotfix corrected listing storage costs

--- src/utils/near-costs.ts
+++ src/utils/near-costs.ts
@@ -1,5 +1,5 @@
-const COST_PER_BYTE = 1e-19
-const MARKET_LIST_COST = 0.0035
+const COST_PER_BYTE = 1e-5
+const MARKET_LIST_COST = 0.0036
 const APPROVAL_STORAGE = 0.0008
 
 export const calculateListCost = (amount: number): number => {
