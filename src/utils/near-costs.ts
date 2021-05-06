10e74d90e44ffe6af6ca2bcc55af42d214a8075a
Luís Freitas <microchipgnu@gmail.com>
2021-05-06 18:44:20 +0100

    fixed list cost

--- src/utils/near-costs.ts
+++ src/utils/near-costs.ts
@@ -3,5 +3,6 @@ const MARKET_LIST_COST = 0.0036
 const APPROVAL_STORAGE = 0.0008
 
 export const calculateListCost = (amount: number): number => {
-  return (APPROVAL_STORAGE * COST_PER_BYTE + MARKET_LIST_COST) * amount
+  // return (APPROVAL_STORAGE * COST_PER_BYTE + MARKET_LIST_COST) * amount
+  return 0.1
 }
