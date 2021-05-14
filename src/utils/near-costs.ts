806b665e314cb92c094c387bbc9d1e6b1951b334
Luís Freitas <microchipgnu@gmail.com>
2021-05-14 14:58:35 +0100

    add storage calcs

--- src/utils/near-costs.ts
+++ src/utils/near-costs.ts
@@ -6,3 +6,11 @@ export const calculateListCost = (amount: number): number => {
   // return (APPROVAL_STORAGE * COST_PER_BYTE + MARKET_LIST_COST) * amount
   return 0.1
 }
+
+export const calculateTransferCost = (): number => {
+  return 0.1
+}
+
+export const calculateMintCost = (): number => {
+  return 0.1
+}
