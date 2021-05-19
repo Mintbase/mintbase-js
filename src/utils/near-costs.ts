d640cb59dda59817e8bf67651d0fb9f9b63d2f42
Luís Freitas <microchipgnu@gmail.com>
2021-05-19 15:18:26 +0100

    adding storage calculation when listing

--- src/utils/near-costs.ts
+++ src/utils/near-costs.ts
@@ -1,16 +1,5 @@
-const COST_PER_BYTE = 1e-5
-const MARKET_LIST_COST = 0.0036
-const APPROVAL_STORAGE = 0.0008
-
+// Returns cost in NEAR
 export const calculateListCost = (amount: number): number => {
-  // return (APPROVAL_STORAGE * COST_PER_BYTE + MARKET_LIST_COST) * amount
-  return 0.1
-}
-
-export const calculateTransferCost = (): number => {
-  return 0.1
-}
-
-export const calculateMintCost = (): number => {
-  return 0.1
+  const cost = 0.0044 * amount
+  return cost
 }
