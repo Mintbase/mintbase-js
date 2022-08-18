57f4d59ba99aba7f5134b945916264798b953b22
Till <till.friesewinkel@googlemail.com>
2022-08-18 14:03:24 +0100

    Enabling empty base URI for more flexibility

--- src/wallet.ts
+++ src/wallet.ts
@@ -978,7 +978,7 @@ export class Wallet {
         name: storeId.replace(/[^a-z0-9]+/gim, '').toLowerCase(),
         symbol: symbol.replace(/[^a-z0-9]+/gim, '').toLowerCase(),
         icon: options?.icon ?? MINTBASE_32x32_BASE64_DARK_LOGO,
-        base_uri: this.constants.BASE_ARWEAVE_URI || BASE_ARWEAVE_URI,
+        base_uri: null,
         reference: null,
         reference_hash: null,
       },
@@ -1098,10 +1098,12 @@ export class Wallet {
       throw new Error(ERROR_MESSAGES.invalidRoyalties)
     }
 
+    const { base_uri } = await contract.nft_metadata()
+
     const obj = {
       owner_id: accountId,
       metadata: {
-        reference: metadataId,
+        reference: base_uri ? metadataId : `${BASE_ARWEAVE_URI}/${metadataId}`,
         // TODO: check if category is lowercase
         extra: !category ? null : category,
       },
@@ -1207,10 +1209,12 @@ export class Wallet {
       {}
     )
 
+    const { base_uri } = await contract.nft_metadata()
+
     const obj = {
       owner_id: accountId,
       metadata: {
-        reference: metaId,
+        reference: base_uri ? metaId : `${BASE_ARWEAVE_URI}/${metaId}`,
         extra: memo,
       },
       num_to_mint: amount,
