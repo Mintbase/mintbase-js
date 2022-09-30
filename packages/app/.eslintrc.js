c18444a52d4c7c41f40931780472ae8086d6383c
cif <email@benipsen.com>
2022-09-30 12:30:07 +0100

    🔧 Omit next web vitals altogether

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   'extends': [
     '../../.eslintrc.json',
-    'next/core-web-vitals',
+    //'next/core-web-vitals',
   ],
   'parserOptions': {
     'project': path.resolve(__dirname, '../../tsconfig.lint.json'),
