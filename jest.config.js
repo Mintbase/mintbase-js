67cb0f9be6fff65aee3fb0f092558115f56542d7
cif <email@benipsen.com>
2022-09-30 15:46:22 +0100

    🔧 Attempt to build first,

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coveragePathIgnorePatterns: ['lib/', 'constants.ts'],
-  modulePathIgnorePatterns: ['lib/'],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  modulePaths: [
-    '/',
-    '/node_modules',
-    '<rootDir>/../../'
-  ]
+  modulePathIgnorePatterns: ['lib/']
 };
