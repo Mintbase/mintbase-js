a42cfab430d413324619b3364444e0ad7afdcfe9
cif <email@benipsen.com>
2022-09-30 16:03:27 +0100

    🔧 Jiggle app linting config

--- packages/app/.eslintrc.js
+++ packages/app/.eslintrc.js
@@ -1,11 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require('path');
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+//const path = require('path');
+// eslint-disable-next-line no-undef
 module.exports = {
   'extends': [
     '../../.eslintrc.json',
     //'next/core-web-vitals',
   ],
-  'parserOptions': {
-    'project': path.resolve(__dirname, '../../tsconfig.lint.json'),
-  },
+  // 'parserOptions': {
+  //   'project': path.resolve(__dirname, '../../tsconfig.lint.json'),
+  // },
 };
