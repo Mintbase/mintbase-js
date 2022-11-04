a0e93b6b10329ee34a0a46355fd6a98f919d938d
Luís Freitas <microchipgnu@gmail.com>
2022-11-04 03:21:42 +0000

    add react hooks for demo

---
+++ packages/react/src/hooks/useMinter.ts
@@ -0,0 +1,54 @@
+import { useWallet } from '../WalletContext';
+import { callContractMethod, MAX_GAS, mint } from '@mintbase-js/sdk';
+import { useState } from 'react';
+import { MintArgs } from '@mintbase-js/sdk/lib/mb-impl/token.types';
+
+export type MinterHookReturn = {
+  loading: boolean;
+  error: any;
+  data: any;
+  methods: any;
+};
+
+export type MinterHookArgs = MintArgs;
+
+export const useMinter = (args: MintArgs): MinterHookReturn => {
+  const { wallet, activeAccountId } = useWallet();
+  const { nftContractId, options } = args;
+
+  const [loading, setLoading] = useState(false);
+
+  const handleMint = async (): Promise<void> => {
+    setLoading(true);
+
+    // TODO: use @mintbase/storage to get a reference
+    const reference = '';
+    
+    await callContractMethod(
+      {
+        ...mint({
+          nftContractId,
+          metadata: {
+            reference: reference,
+          },
+          options,
+        }),
+        gas: MAX_GAS,
+        deposit: '0',
+        signerId: activeAccountId,
+      },
+      { wallet },
+    );
+
+    setLoading(false);
+  };
+
+  return {
+    loading: loading,
+    error: null,
+    data: null,
+    methods: {
+      mint: handleMint,
+    },
+  };
+};
--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,2 +1,4 @@
 export * from './calls';
 export * from './constants';
+export * from './mb-impl/token';
+export * from './mb-impl/market';
