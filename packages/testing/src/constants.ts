2ebccdaf7128ee9ffb5c9d4520ff03a459bd7b24
cif <email@benipsen.com>
2022-11-14 11:30:57 +0000

    📦 Add testing package starting point

---
+++ packages/testing/src/constants.ts
@@ -0,0 +1,1 @@
+export const SECRETS_REPO_PATH = 'projects/185858989453/secrets/';
---
+++ packages/testing/tests/transfer.test.ts
@@ -0,0 +1,5 @@
+describe('a soon to be filled in test', () => {
+  it('does a thing', () => {
+    expect(true).toBeTruthy();
+  });
+});
