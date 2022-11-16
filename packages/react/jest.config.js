30ffdb599346b5137af747c9bb02172a87098f42
cif <email@benipsen.com>
2022-11-16 15:19:28 +0000

    🔧 Make jest happy with stub in react package

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   ...require('../../jest.config.js'),
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "@mintbase-js/auth": "<rootDir>/../../packages/auth/src/"
-  }
+  testEnvironment: "jsdom"
 }
