b105acbbd0ef89ab154cd67288e8c44ccef47a14
Luís Freitas <microchipgnu@gmail.com>
2022-11-17 16:39:06 +0000

    fix: webpack fs on next js (#173)

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  webpack: (config) =>{
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
