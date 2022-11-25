ff808468e5676686bfc6df49279ad15726ba83b3
Till <till.friesewinkel@googlemail.com>
2022-11-25 15:54:50 +0000

    Remove logging, reorder (failure > pending)

--- packages/rpc/src/methods/txnstatus.ts
+++ packages/rpc/src/methods/txnstatus.ts
@@ -1,15 +1,15 @@
-import { requestFromNearRpc } from '../util';
+import { requestFromNearRpc } from "../util";
 
-export type TxnStatus = 'pending' | 'success' | 'failure';
+export type TxnStatus = "pending" | "success" | "failure";
 
 export const getTxnStatus = async (
   txnHash: string,
-  senderId: string,
+  senderId: string
 ): Promise<TxnStatus> => {
   const res = await requestFromNearRpc({
-    jsonrpc: '2.0',
-    id: 'dontcare',
-    method: 'tx',
+    jsonrpc: "2.0",
+    id: "dontcare",
+    method: "tx",
     params: [txnHash, senderId],
   });
   if (res.error) throw res.error;
@@ -17,11 +17,10 @@ export const getTxnStatus = async (
   let failure = false;
   let pending = false;
   res.result.receipts_outcome.forEach((outcome: any) => {
-    console.log(outcome);
     if (outcome.outcome.status.Unknown) pending = true;
     if (outcome.outcome.status.Failure) failure = true;
   });
-  if (pending) return 'pending';
-  if (failure) return 'failure';
-  return 'success';
+  if (failure) return "failure";
+  if (pending) return "pending";
+  return "success";
 };
