fd432d26d8054ef124c2cebae6959e3b1594ec2f
Ruben Marcus <ruben@rubenmarcus.dev>
2022-11-28 16:00:51 +0000

    useOwnedNftsByStores && ownedNftsByStore

--- packages/data/src/api/index.ts
+++ packages/data/src/api/index.ts
@@ -2,4 +2,4 @@ export * from './tokenListingCounts/tokenListingCountsByMetaId';
 export * from './ownedTokens/ownedTokens';
 export * from './tokenById/tokenById';
 export * from './tokensByStatus/tokensByStatus';
-
+export * from './ownedNftsByStore/ownedNftsByStore';
---
+++ packages/data/src/api/ownedNftsByStore/ownedNftsByStore.ts
@@ -0,0 +1,34 @@
+import { fetchGraphQl } from '../../graphql/fetch';
+import { ownedNftsByStoreQuery } from './ownedNftsByStore.query';
+import { NftsByStoreData, OwnedNftsData } from './ownedNftsByStore.types';
+
+
+export const ownedNftsByStore = async (
+  ownerId: string,
+  pagination: { limit: number; offset?: number},
+): Promise<NftsByStoreData> => {
+
+
+  const wrongParams = typeof pagination?.limit === 'undefined' || typeof ownerId === 'undefined' || pagination?.limit < 1;
+
+  if (wrongParams) {
+    console.error(`Error fetching nfts from ${ownerId}, please check the arguments provided`);
+    return null;
+  }
+
+  const { data, error } = await fetchGraphQl<OwnedNftsData>({
+    query: ownedNftsByStoreQuery,
+    variables: {
+      ownerId,
+      limit: pagination.limit,
+      offset: pagination?.offset ?? 0,
+    },
+  });
+
+  if (error) {
+    console.error(`Error fetching nfts from ${ownerId}`, error.message);
+    throw error;
+  }
+
+  return { data: data , error:error };
+};
