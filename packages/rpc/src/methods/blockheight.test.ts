22933613dbfdcdbcc79b9f34172e8ff3f93e7098
cif <email@benipsen.com>
2022-11-28 11:17:35 +0000

    ✅ Mock fetch for RPC unit tests

--- packages/rpc/src/methods/blockheight.test.ts
+++ packages/rpc/src/methods/blockheight.test.ts
@@ -1,7 +1,22 @@
 import { getBlockHeight } from './blockheight';
+import fetch from 'isomorphic-unfetch';
+
+jest.mock('isomorphic-unfetch');
 
 describe('getBlockHeight', () => {
   it('should return a numeric, non-negative block height', async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce({
+        result: {
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          sync_info: {
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            latest_block_height: 123,
+          },
+        },
+      }),
+    });
+
     const h = await getBlockHeight();
     expect(typeof h).toBe('number');
     expect(h > 0).toBe(true);
--- packages/rpc/src/methods/txnstatus.test.ts
+++ packages/rpc/src/methods/txnstatus.test.ts
@@ -1,7 +1,27 @@
 import { getTxnStatus } from './txnstatus';
+import fetch from 'isomorphic-unfetch';
+
+jest.mock('isomorphic-unfetch');
 
 describe('getTxnStatus', () => {
+
+  const mockResponseWithReceipts = (receipts_outcome: object): void => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce({
+        result: {
+          // eslint-disable-next-line @typescript-eslint/camelcase
+          receipts_outcome,
+        },
+      }),
+    });
+  };
+
   it('recognizes successful transactions (multiple receipts)', async () => {
+    // mock response
+    mockResponseWithReceipts([
+      { outcome: { status: true } },
+      { outcome: { status: true } },
+    ]);
     let txnHash = '9Nieji1G1d9iUmkMindAAdwz7mcESG2NFxredjv9cWrZ';
     let senderId = 'reginanogueira.near';
     if (process.env.NEAR_ENV === 'testnet') {
@@ -13,6 +33,9 @@ describe('getTxnStatus', () => {
   });
 
   it('recognizes successful transactions (single receipt)', async () => {
+    mockResponseWithReceipts([
+      { outcome: { status: true } },
+    ]);
     let txnHash = 'DfePCY1bRdCn8BsBjod4kP3sW6Uc1VYV4yJxnFNy5Vui';
     let senderId = 'yoneru.near';
     if (process.env.NEAR_ENV === 'testnet') {
@@ -23,5 +46,19 @@ describe('getTxnStatus', () => {
     expect(status).toStrictEqual('success');
   });
 
-  // TODO: failed transactions
+  it('recognizes failed transactions', async () => {
+    mockResponseWithReceipts([
+      { outcome: { status:  { Failure: true } } },
+    ]);
+    let txnHash = 'test.txn.hash';
+    let senderId = 'foo.near';
+    if (process.env.NEAR_ENV === 'testnet') {
+      txnHash = 'not.a.real.txn';
+      senderId = 'bar.testnet';
+    }
+    const status = await getTxnStatus(txnHash, senderId);
+    expect(status).toStrictEqual('failure');
+  });
+
+  // TODO: more coverages.
 });
