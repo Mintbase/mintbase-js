fb57733b715f86cc25d8450b524ee53d46ec30a6
cif <email@benipsen.com>
2022-11-29 23:12:54 +0000

    🚚 Isolate near api js in auth module

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,3 +1,12 @@
 export * from './account';
 export * from './wallet';
 export * from './constants';
+
+// this is done to avoid importing near-api-js more than once
+// which leads to a strange, but known issue
+// https://docs.near.org/tools/near-api-js/faq#class-x-is-missing-in-schema-publickey
+export { Account, providers, connect as connectToNear, KeyPair } from 'near-api-js';
+export { InMemoryKeyStore, KeyStore } from 'near-api-js/lib/key_stores';
+
+// adding some other types from within libraries as a convenience
+export { FinalExecutionOutcome } from '@near-wallet-selector/core';
