5593fbc0c029a9d7c826e1682d0c3a6c7a8efeba
Sérgio <sergioseco98@gmail.com>
2022-12-02 10:57:12 +0000

    📝 Deposit + List integration tests

---
+++ packages/testing/tests/SDK/deposit.integration.test.ts
@@ -0,0 +1,23 @@
+import { depositStorage } from '@mintbase-js/sdk/src';
+import { execute } from '@mintbase-js/sdk/src';
+import { connect, FinalExecutionOutcome } from '@mintbase-js/auth';
+import { authenticatedKeyStore } from '../../src/utils';
+
+test('deposit storage for current account', async () => {
+  const accounts = ['mb_alice.testnet', 'mb_bob.testnet'];
+  const burnFromIndex = Math.random() > 0.5 ? 1 : 0;
+  const accountToDepositFrom = accounts[burnFromIndex];
+  const keyStore = await authenticatedKeyStore([accountToDepositFrom]);
+  const signingAccount = await connect(accountToDepositFrom, keyStore);
+
+  const result = (await execute(
+    {
+      ...depositStorage({}),
+    },
+    { account: signingAccount },
+  )) as FinalExecutionOutcome; 
+
+
+  expect(result.receipts_outcome).not.toBeUndefined();
+
+});
---
+++ packages/testing/tests/SDK/list.integration.test.ts
@@ -0,0 +1,52 @@
+import { TEST_TOKEN_CONTRACT } from '../../src/constants';
+import { list } from '@mintbase-js/sdk/src';
+import { execute } from '@mintbase-js/sdk/src';
+import { tokensByStatus } from '@mintbase-js/data/src/api/tokensByStatus/tokensByStatus';
+import { connect, FinalExecutionOutcome } from '@mintbase-js/auth';
+import { authenticatedKeyStore } from '../../src/utils';
+import { ownedTokens } from '@mintbase-js/data';
+
+test('list a token', async () => {
+  const accounts = ['mb_alice.testnet', 'mb_bob.testnet'];
+  const burnFromIndex = Math.random() > 0.5 ? 1 : 0;
+  const accountToListFrom = accounts[burnFromIndex];
+  const keyStore = await authenticatedKeyStore([accountToListFrom]);
+  const signingAccount = await connect(accountToListFrom, keyStore);
+  const token = await ownedTokens(accountToListFrom, { limit: 1 });
+
+  //   const deposit = (await execute(
+  //     {
+  //       ...depositStorage({
+  //         marketAddress: MB_TESTNET_MARKET_CONTRACT_ADDRESS,
+  //       }),
+  //     },
+  //     { account: signingAccount },
+  //   )) as FinalExecutionOutcome; 
+
+  if (!token) {
+    throw `${accountToListFrom} ran out of owned tokens to burn! Mint some more...`;
+  }
+  const {  unlistedTokens } = await tokensByStatus(
+    'mb_store.mintspace2.testnet:285633c53dbad8e3493a39849f29092d',
+    accountToListFrom,
+  );
+
+  const tokenToList: string = unlistedTokens[0];
+
+  if (tokenToList.length < 0) {
+    console.error('No unburned tokens for list integration test');
+    return;
+  }
+
+  const result = (await execute(
+    list({
+      nftContractId: TEST_TOKEN_CONTRACT,
+      tokenId: tokenToList,
+      price: '1',
+    }),
+    { account: signingAccount },
+  )) as FinalExecutionOutcome;
+
+  expect(result.receipts_outcome).not.toBeUndefined();
+
+});
