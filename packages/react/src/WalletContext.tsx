3d35d323f49cb278bdb1da50e84f236e3fa6f39d
Ruben Marcus <ruben@rubenmarcus.dev>
2022-12-05 19:14:30 +0000

    add isWaitingforConnection as false

--- packages/react/src/WalletContext.tsx
+++ packages/react/src/WalletContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useContext, useEffect, useLayoutEffect, useState } from 'react';
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 import {
   registerWalletAccountsSubscriber,
   setupWalletSelectorComponents,
