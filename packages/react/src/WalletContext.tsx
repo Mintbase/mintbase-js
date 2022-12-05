531247ca9647e7a9580144ee37b538084a76590e
cif <email@benipsen.com>
2022-12-05 22:41:47 +0000

    fix: :rotating_light: Fix merge, add warning

--- packages/react/src/WalletContext.tsx
+++ packages/react/src/WalletContext.tsx
@@ -63,7 +63,11 @@ export const WalletContextProvider: React.FC<React.PropsWithChildren> = (
 
   if (typeof window !== 'undefined') {
     const closeButton = document?.getElementsByClassName('close-button')[0];
-    closeButton && closeButton?.addEventListener('click', onCloseModal);
+    if (!closeButton) {
+      console.warn('Attempting to bind handler to non-existent className .close-button');
+    } else {
+      closeButton?.addEventListener('click', onCloseModal);
+    }
   }
 
   // subscribe to account state changes
@@ -71,7 +75,7 @@ export const WalletContextProvider: React.FC<React.PropsWithChildren> = (
     if (!components) {
       return;
     }
- 
+
     const subscription = registerWalletAccountsSubscriber(
       (accounts: AccountState[]) => {
         setAccounts(accounts);
