2987873cacb0abb879714d2ad8d309036b828c38
Sérgio <sergioseco98@gmail.com>
2022-12-06 17:02:19 +0000

    ♻️v1 method namespace attempt

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,20 +1,17 @@
 export * from './execute';
 export * from './constants';
-// export * from './burn/burn';
-// export * from './addMinter/addMinter';
-// export * from './removeMinter/removeMinter';
-// export * from './transfer/transfer';
-// export * from './batchChangeMinters/batchChangeMinters';
-// export * from './deployContract/deployContract';
-// export * from './transferContractOwnership/transferContractOwnership';
-// export * from './list/list';
-// export * from './buy/buy';
-// export * from './depositStorage/depositStorage';
-// export * from './mint/mint';
-// export * from './revoke/revoke';
-export * from './v1/token';
-export * from './v1/token.types';
-export * from './v1/market';
-export * from './v1/market.types';
+export * from './burn/burn';
+export * from './addMinter/addMinter';
+export * from './removeMinter/removeMinter';
+export * from './transfer/transfer';
+export * from './batchChangeMinters/batchChangeMinters';
+export * from './deployContract/deployContract';
+export * from './transferContractOwnership/transferContractOwnership';
+export * from './list/list';
+export * from './buy/buy';
+export * from './depositStorage/depositStorage';
+export * from './mint/mint';
+export * from './revoke/revoke';
+export *  as v1 from './v1';
 
 
