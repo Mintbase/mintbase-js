f0e4fce2fa188c7c4d849d4a6cad7133565e9dd7
Sérgio <sergioseco98@gmail.com>
2022-12-22 18:46:58 +0000

    📝 Delist integration test

---
+++ packages/testing/tests/SDK/delist.integration.test.ts
@@ -0,0 +1,42 @@
+import { delist } from '@mintbase-js/sdk/src';
+import { execute } from '@mintbase-js/sdk/src';
+import { tokensByStatus } from '@mintbase-js/data/src/api/tokensByStatus/tokensByStatus';
+import { connect, FinalExecutionOutcome } from '@mintbase-js/auth';
+import { authenticatedKeyStore } from '../../src/utils';
+import { ownedTokens } from '@mintbase-js/data';
+import { TEST_TOKEN_CONTRACT } from '../../src/constants';
+
+test('delist a token', async () => {
+  const accounts = ['mb_alice.testnet', 'mb_bob.testnet'];
+  const listFromIndex = Math.random() > 0.5 ? 1 : 0;
+  const accountToListFrom = accounts[listFromIndex];
+  const keyStore = await authenticatedKeyStore([accountToListFrom]);
+  const signingAccount = await connect(accountToListFrom, keyStore);
+  const token = await ownedTokens(accountToListFrom, { limit: 1 });
+
+  if (!token) {
+    throw `${accountToListFrom} ran out of owned tokens to list! Mint some more...`;
+  }
+
+  const { listedTokens } = await tokensByStatus(
+    token[0].metadataId,
+    accountToListFrom,
+  );
+
+  if (listedTokens.length <= 0) {
+    console.error('No listed tokens for list integration test');
+    return;
+  }
+
+  const result = (await execute(
+    { account: signingAccount },
+    delist({
+      tokenIds: [listedTokens[0]],
+      nftContractId: TEST_TOKEN_CONTRACT,
+    }),
+  )) as FinalExecutionOutcome[];
+
+  expect(result[0].receipts_outcome).not.toBeUndefined();
+  expect(result[1].receipts_outcome).not.toBeUndefined();
+
+});
