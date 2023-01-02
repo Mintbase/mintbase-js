4a02e1cf2aa19d58a3a9cb3429a53125d1ee8c25
Ruben Marcus <ruben@rubenmarcus.dev>
2023-01-02 16:57:38 +0000

    resolve conflicts

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,4 @@
 export * from './WalletContext';
-export * from './hooks/useMinter';
+// export * from './hooks/useMinter';
 export * from './hooks/useTokenById';
 export * from './hooks/useOwnedNftsByStores';
