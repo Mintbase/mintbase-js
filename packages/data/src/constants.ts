03e92dcba0cec007620681b54dcf8eecfd375173
Ruben Marcus <ruben@rubenmarcus.dev>
2023-01-05 12:41:18 +0000

    fix constants

--- packages/data/src/constants.ts
+++ packages/data/src/constants.ts
@@ -1,4 +1,3 @@
-import { mbjs } from '@mintbase-js/sdk';
 
 export type NearDataEnv = 'mainnet' | 'testnet' | 'sandbox';
 
@@ -18,8 +17,7 @@ if (globalThis?.window?.location.host.includes(DataNetwork.SANDBOX)) {
   hostBasedNetworkDefault = DataNetwork.SANDBOX;
 }
 
-export const NEAR_DATA_ENV = mbjs.env.network ||
-  hostBasedNetworkDefault;
+export const NEAR_DATA_ENV = hostBasedNetworkDefault;
 
 export const GRAPHQL_ENDPOINT = `https://interop-${NEAR_DATA_ENV}.hasura.app/v1/graphql`;
 export const QUERY_OPS_PREFIX = 'mintbase_js_data_';
