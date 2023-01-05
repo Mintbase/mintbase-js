4714726ca3854feda0e8c6f5f15bf678808c7be6
ruisantiago <ruisantiagomr@gmail.com>
2023-01-05 09:56:02 +0000

    remove unused file

--- packages/data/src/api/tokenOwner/tokenOwner.errors.ts
+++
@@ -1,9 +0,0 @@
-export const errorContractAddress = {
-  message: 'Please add a valid contractAddress as string',
-  valid: false,
-};
-
-export const errorToken = {
-  message: 'Please add a valid tokenId as string',
-  valid: false,
-};
