24e548074e60b1429d2ea9c5848d74e55aa7febf
Ruben Marcus <ruben@rubenmarcus.dev>
2023-01-06 14:17:19 +0000

    fix config object

--- packages/sdk/src/config.ts
+++ packages/sdk/src/config.ts
@@ -2,28 +2,41 @@
 
 // mbjs.config({envvars ... })
 
-import { MBJS_CONFIG_OBJ, NEAR_NETWORK } from './configObj';
+import { MBJS_CONFIG_OBJ, MbJsKeysObject } from './configObj';
 
 declare global {
-  interface Window { mintbase: any }
+  interface Window { mintbase: MbJsKeysObject }
 }
 
-let CONFIG_OBJ: any;
+const CONFIG_KEYS: MbJsKeysObject = { 
+  network: '',
+  graphql_url:  '',
+  callbackUrl: '',
+  isSet: false,
+}; 
 
 export const mbjs = {
-  config: (configObj: MBJS_CONFIG_OBJ): any => { 
+  config: (configObj: MBJS_CONFIG_OBJ): null => { 
     const globalConfig = {
       network: configObj.network || 'testnet',
-      graphql_url: `https://interop-${configObj.network}.hasura.app/v1/graphql` || '',
+      graphqlUrl: `https://interop-${configObj.network}.hasura.app/v1/graphql` || '',
       callbackUrl: configObj.callbackUrl || '',
       isSet: true,
     }; 
-    CONFIG_OBJ = globalConfig;
+
+    CONFIG_KEYS.network = globalConfig.network;
+    CONFIG_KEYS.graphql_url = globalConfig.graphqlUrl;
+    CONFIG_KEYS.callbackUrl = globalConfig.callbackUrl;
+    CONFIG_KEYS.isSet = globalConfig.isSet;
+
+
+    console.log(globalConfig, CONFIG_KEYS, 'keys');
 
     if (globalThis?.window) {
       window.mintbase = globalConfig;
     }
 
+    return null;
   },
-  env: globalThis?.window?.mintbase,
+  keys: CONFIG_KEYS,
 };
--- packages/sdk/src/configObj.ts
+++ packages/sdk/src/configObj.ts
@@ -1,8 +1,12 @@
-export type NEAR_NETWORK = 'testnet' | 'mainnet'
+export type NEAR_NETWORK = 'testnet' | 'mainnet' | 'sandbox'
 
 
 export type MBJS_CONFIG_OBJ = {
-  network: NEAR_NETWORK;
+  network: NEAR_NETWORK | '';
   graphql_url?: string;
   callbackUrl?: string;
+}
+
+export interface MbJsKeysObject extends MBJS_CONFIG_OBJ  {
+ isSet: boolean;
 }
