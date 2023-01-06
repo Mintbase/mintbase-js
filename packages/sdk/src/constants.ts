a5e005c7dc194c63d27e1e957864a514aeb4b7dc
Ruben Marcus <ruben@rubenmarcus.dev>
2023-01-06 17:38:56 +0000

    remove process.envs

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -52,7 +52,6 @@ export const Network = {
 export const MINTBASE_API_KEY_HEADER = 'mb-api-key';
 export const MINTBASE_API_ANON_USER = 'anon';
 export const MINTBASE_API_KEY =
-  process.env.MINTBASE_API_KEY ||
   globalThis?.localStorage?.getItem('MINTBASE_API_KEY') ||
   MINTBASE_API_ANON_USER;
 
@@ -70,7 +69,7 @@ export const MB_MAINNET_TOKEN_FACTORY_ADDRESS = 'mintbase1.near';
 export const MB_TESTNET_TOKEN_FACTORY_ADDRESS = 'mintspace2.testnet';
 export const MB_TESTNET_MARKET_CONTRACT_ADDRESS = 'market-v2-beta.mintspace2.testnet';
 export const MB_MAINNET_MARKET_CONTRACT_ADDRESS = 'simple.market.mintbase1.near';
-export const DEFAULT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ID || null;
+export const DEFAULT_CONTRACT_ADDRESS =  null;
 export const MB_TOKEN_FACTORY_ADDRESS =
   NEAR_NETWORK === Network.MAINNET ? MB_MAINNET_TOKEN_FACTORY_ADDRESS : MB_TESTNET_TOKEN_FACTORY_ADDRESS;
 
