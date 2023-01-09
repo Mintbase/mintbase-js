6ac8574393aa09ff0276cde1f50c375241747985
ruisantiago <ruisantiagomr@gmail.com>
2023-01-09 18:30:54 +0000

    Fix Typo in OwnerId var

--- packages/data/src/api/ownedStores/ownedStores.query.ts
+++ packages/data/src/api/ownedStores/ownedStores.query.ts
@@ -3,8 +3,8 @@ import { QUERY_OPS_PREFIX } from '../../constants';
 
 
 export const ownedStoresQuery = gql`
-query ${QUERY_OPS_PREFIX}_GetOwnedStores($ownedId: String!) {
-  nft_contracts(where: {owner_id: {_eq: $ownedId}}) {
+query ${QUERY_OPS_PREFIX}_GetOwnedStores($ownerId: String!) {
+  nft_contracts(where: {owner_id: {_eq: $ownerId}}) {
     id
   }
 }
