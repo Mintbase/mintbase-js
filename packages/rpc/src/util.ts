325932508721699ad0588fb22788f743dcb3b344
Ruben Marcus <ruben@rubenmarcus.dev>
2023-01-11 20:09:19 +0000

    fix tests

--- packages/rpc/src/util.ts
+++ packages/rpc/src/util.ts
@@ -1,10 +1,10 @@
-import { mbjs, RPC_ENDPOINTS } from '@mintbase-js/sdk';
+import { mbjs } from '@mintbase-js/sdk';
 import fetch from 'isomorphic-unfetch';
 
 export const requestFromNearRpc = async (
   body: Record<string, any>,
 ): Promise<Record<string, any> | undefined> => {
-  const fetchUrl = RPC_ENDPOINTS[mbjs.keys.network] ?? mbjs.keys.nearRpcUrl;
+  const fetchUrl =  mbjs.keys.nearRpcUrl;
 
   const res = await fetch(fetchUrl, {
     method: 'POST',
