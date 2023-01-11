33084d3e4fbe2fa7752eb3488de1db1f2ee6d1ab
Ruben Marcus <ruben@rubenmarcus.dev>
2023-01-11 19:58:20 +0000

    remove rpc consts

--- packages/rpc/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export type NearEnv = 'mainnet' | 'testnet';
-export const NEAR_NETWORK =
-  process.env.NEAR_NETWORK || process.env.NEXT_PUBLIC_NEAR_NETWORK || 'mainnet';
-export const NEAR_RPC_URL = `https://rpc.${NEAR_NETWORK}.near.org`;
