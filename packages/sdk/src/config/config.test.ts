a5b977a8660d2fa04cb6df61dc9945e9f88ee80a
Ruben Marcus <ruben@rubenmarcus.dev>
2023-01-13 14:37:01 +0000

    add js dom env

--- packages/sdk/src/config/config.test.ts
+++ packages/sdk/src/config/config.test.ts
@@ -9,47 +9,6 @@ describe('test mbjs namespace', () => {
     jest.resetModules();
   });
 
-  it('should set CONFIG_KEYS with testnet values', () => {
-
-
-    jest.spyOn(methods, 'setGlobalEnv');
-
-
-    const config: MBJS_CONFIG_PARAMS = {
-      network: NEAR_NETWORKS.TESTNET,
-      contractAddress:  TESTNET_MOCK.contractAddress,
-      callbackUrl: TESTNET_MOCK.callbackUrl,
-    };
-
-    methods.mbjs.config(config);
-
-
-    expect(methods.setGlobalEnv).toHaveBeenCalledWith(config);
-    expect(methods.mbjs.keys).toEqual(TESTNET_MOCK);
-   
-  });
-
-
-  it('should set CONFIG_KEYS with mainnet values', () => {
-
-
-    jest.spyOn(methods, 'setGlobalEnv');
-
-
-    const config: MBJS_CONFIG_PARAMS = {
-      network: NEAR_NETWORKS.MAINNET,
-      contractAddress:  MAINNET_MOCK.contractAddress,
-      callbackUrl: MAINNET_MOCK.callbackUrl,
-    };
-
-    methods.mbjs.config(config);
-
-
-    expect(methods.setGlobalEnv).toHaveBeenCalledWith(config);
-    expect(methods.mbjs.keys).toEqual(MAINNET_MOCK);
-   
-  });
-
 
   it('should set CONFIG_KEYS with process.env TESTNET', () => {
     process.env.NEAR_NETWORK = NEAR_NETWORKS.TESTNET;
@@ -71,7 +30,6 @@ describe('test mbjs namespace', () => {
     process.env.CONTRACT_ADDRESS = MAINNET_MOCK.contractAddress;
     process.env.CALLBACK_URL = MAINNET_MOCK.callbackUrl;
 
-    // windowSpy.mockImplementation(() => undefined);
 
     // had to require module because jest was running the file before process.env be set on the test so it always failed.
     // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -81,6 +39,57 @@ describe('test mbjs namespace', () => {
     expect(process.env.NEAR_NETWORK).toBeDefined();
 
     expect(configModule.mbjs.keys).toEqual(MAINNET_MOCK);
+  });
+
+
+  /**
+ * @testEnvironment jsdom
+ */
+
+  it('should set CONFIG_KEYS with testnet values', () => {
+
+
+    jest.spyOn(methods, 'setGlobalEnv');
+
+
+    const config: MBJS_CONFIG_PARAMS = {
+      network: NEAR_NETWORKS.TESTNET,
+      contractAddress:  TESTNET_MOCK.contractAddress,
+      callbackUrl: TESTNET_MOCK.callbackUrl,
+    };
+
+    methods.mbjs.config(config);
+
+
+    expect(methods.setGlobalEnv).toHaveBeenCalledWith(config);
+    expect(methods.mbjs.keys).toEqual(TESTNET_MOCK);
+   
+  });
+
+
+  /**
+ * @testEnvironment jsdom
+ */
+
+
+  it('should set CONFIG_KEYS with mainnet values', () => {
+
+
+    jest.spyOn(methods, 'setGlobalEnv');
+
+
+    const config: MBJS_CONFIG_PARAMS = {
+      network: NEAR_NETWORKS.MAINNET,
+      contractAddress:  MAINNET_MOCK.contractAddress,
+      callbackUrl: MAINNET_MOCK.callbackUrl,
+    };
+
+    methods.mbjs.config(config);
+
+
+    expect(methods.setGlobalEnv).toHaveBeenCalledWith(config);
+    expect(methods.mbjs.keys).toEqual(MAINNET_MOCK);
+   
   });
 
 });
