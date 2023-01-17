0235bbfb5c3557957f369bcf2aad3b10d2347f25
ruisantiago <ruisantiagomr@gmail.com>
2023-01-17 12:19:18 +0000

    Add metadataByMetadaId method in data package

---
+++ packages/data/src/api/metadataByMetadataId/metadataByMetadataId.test.ts
@@ -0,0 +1,35 @@
+import { GraphQLClient } from 'graphql-request';
+import { metadataByMetadataId } from './metadataByMetadataId';
+import { metadataByMetadataIdMock } from './metadataByMetadataId.mock';
+import { MetadataByMetadataIdQueryResult } from './metadataByMetadataId.types';
+
+jest.mock('graphql-request');
+
+describe('getMetadataByMetadataId', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {
+      // console.log('Suppressed console error.');
+    });
+  });
+
+  it('returns data', async () => {
+    (GraphQLClient as jest.Mock).mockImplementationOnce(() => ({
+      request: (): Promise<MetadataByMetadataIdQueryResult> =>
+        Promise.resolve(metadataByMetadataIdMock),
+    }));
+    const result = await metadataByMetadataId('test' );
+    expect(result?.data).toStrictEqual(metadataByMetadataIdMock);
+  });
+
+  it('should handle errors', async () => {
+    const errMessage = 'exploded';
+    (GraphQLClient as jest.Mock).mockImplementationOnce(() => ({
+      request: (): Promise<MetadataByMetadataIdQueryResult> =>
+        Promise.reject(new Error(errMessage)),
+    }));
+
+    const call = await metadataByMetadataId('test');
+
+    expect(call).toStrictEqual({ error: errMessage });
+  });
+});
