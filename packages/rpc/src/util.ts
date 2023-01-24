b0560bcea277d222d1b52099360e3ea237603eab
Till <31419678+tifrel@users.noreply.github.com>
2023-01-24 10:32:25 +0000

    feat: Add method to resolve payouts from RPC for UI (#277)
    Co-authored-by: cif <email@benipsen.com>

--- packages/rpc/src/util.ts
+++ packages/rpc/src/util.ts
@@ -4,7 +4,7 @@ import fetch from 'isomorphic-unfetch';
 export const requestFromNearRpc = async (
   body: Record<string, any>,
 ): Promise<Record<string, any> | undefined> => {
-  const fetchUrl = RPC_ENDPOINTS[mbjs.keys.network] ||  mbjs.keys.nearRpcUrl;
+  const fetchUrl = RPC_ENDPOINTS[mbjs.keys.network] || mbjs.keys.nearRpcUrl;
 
   const res = await fetch(fetchUrl, {
     method: 'POST',
@@ -13,4 +13,38 @@ export const requestFromNearRpc = async (
   });
 
   return res.json();
+};
+
+export const callViewMethod = async <T>({
+  contractId,
+  method,
+  args,
+}: {
+  contractId: string;
+  method: string;
+  args?: Record<string, any>;
+}): Promise<T> => {
+  const args_base64 = args
+    ? Buffer.from(JSON.stringify(args), 'utf-8').toString('base64')
+    : '';
+
+  const res = await requestFromNearRpc({
+    jsonrpc: '2.0',
+    id: 'dontcare',
+    method: 'query',
+    params: {
+      request_type: 'call_function',
+      finality: 'final',
+      account_id: contractId,
+      method_name: method,
+      args_base64,
+    },
+  });
+
+  if (res?.error) {
+    throw res.error;
+  }
+
+  const parsed = JSON.parse(Buffer.from(res?.result?.result).toString());
+  return parsed as T;
 };
