d7fa2f83021ed151833f261af80c5fcd8c65352e
cif <email@benipsen.com>
2023-02-17 08:14:22 +0000

    feat(sdk): :sparkles: Support specifying token ids

--- packages/testing/src/constants.ts
+++ packages/testing/src/constants.ts
@@ -1,2 +1,3 @@
 export const SECRETS_REPO_PATH = 'projects/185858989453/secrets/';
 export const TEST_TOKEN_CONTRACT = 'mb_store.mintspace2.testnet';
+export const LICENSE_TEST_TOKEN_CONTRACT = 'creativelicense2.testnet';
