a0fa65b1e55606447d13426c0264fe9e4804e351
cif <email@benipsen.com>
2023-02-22 11:51:26 +0000

    feat(react): :sparkles: Add useNearPrice hook to mbjs

---
+++ packages/react/src/hooks/useNearPrice.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { nearPrice  } from '@mintbase-js/data';
+
+type UseNearPriceReturn = {
+  nearPrice: number;
+  error: string;
+}
+
+export const useNearPrice = (): UseNearPriceReturn => {
+  const [price, setPrice] = useState<number>(0);
+  const [error, setError] = useState<string>(null);
+
+  useEffect(() => {
+    const nearPriceData = async (): Promise<void> => {
+      const { data, error } = await nearPrice();
+      if (error) {
+        setError(error);
+        return;
+      }
+      setPrice(Number(data));
+    };
+    nearPriceData();
+  }, []);
+
+  return {
+    nearPrice: price,
+    error,
+  };
+};
