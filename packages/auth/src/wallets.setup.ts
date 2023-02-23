a7f914f34abe735f5afbef89e4fd85aaf537fa10
Ruben Marcus <ruben@rubenmarcus.dev>
2023-02-23 12:08:06 +0000

    add my near wallet

--- packages/auth/src/wallets.setup.ts
+++ packages/auth/src/wallets.setup.ts
@@ -2,6 +2,8 @@ import { setupNearWallet } from '@near-wallet-selector/near-wallet';
 import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
 import { setupSender } from '@near-wallet-selector/sender';
 import { setupHereWallet } from '@near-wallet-selector/here-wallet';
+import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
+
 import { WalletModuleFactory } from '@near-wallet-selector/core';
 
 export const SUPPORTED_NEAR_WALLETS: Array<WalletModuleFactory> =[
@@ -9,4 +11,5 @@ export const SUPPORTED_NEAR_WALLETS: Array<WalletModuleFactory> =[
   setupMeteorWallet(),
   setupSender(),
   setupHereWallet(),
+  setupMyNearWallet(),
 ];
