f69678ff839392c7deec55d94216e4514da5b392
cif <email@benipsen.com>
2023-02-27 10:47:35 -0700

    refactor(data): :recycle: Refactor nearPrice data method to use promise.any

--- packages/data/src/api/nearPrice/nearPrice.ts
+++ packages/data/src/api/nearPrice/nearPrice.ts
@@ -1,40 +1,37 @@
+import fetch from 'isomorphic-unfetch';
 import { BINANCE_API, COIN_GECKO_API } from '../../constants';
 import { ParsedDataReturn } from '../../types';
 import { parseData } from '../../utils';
 import {
-  BinanceNearPriceData,
+  NearPriceData,
   CoinGeckoNearPriceData,
-  NearPriceError,
 } from './nearPrice.types';
 
-export const nearPriceFallback = async (): Promise<CoinGeckoNearPriceData> => {
-  const geckoApi = await fetch(COIN_GECKO_API);
-  const data = geckoApi.json();
+
+export const fetchPriceFromCoinGecko = async (): Promise<NearPriceData> => {
+  const req = await fetch(COIN_GECKO_API);
+  const data = await req.json() as CoinGeckoNearPriceData;
+  return { price: data.near.usd };
+};
+
+export const fetchPriceFromBinance = async (): Promise<NearPriceData> => {
+  const req = await fetch(BINANCE_API);
+  const data = await req.json() as NearPriceData;
   return data;
 };
 
-const nearPriceData = async (): Promise<string | NearPriceError> => {
-  try {
-    const req = await fetch(BINANCE_API);
-
-    const res: BinanceNearPriceData = await req.json();
-
-    if (!res.price) {
-      const price = await nearPriceFallback();
-      return price.near.usd;
-    }
-
-    return res.price;
-  } catch (error) {
-    return { error: 'Error fetching NEAR price' };
-  }
-};
-
 export const nearPrice = async (): Promise<ParsedDataReturn<string>> => {
-  const price = await nearPriceData();
-  const validPrice = typeof price === 'string';
+  try {
+    const res = await Promise.any([
+      fetchPriceFromCoinGecko(),
+      fetchPriceFromBinance(),
+    ]);
 
-  const errorMsg = !validPrice ? 'Error fetching NEAR price' : '';
-
-  return parseData(validPrice ? price : '', errorMsg, errorMsg);
+    return parseData(res.price);
+  } catch (err) {
+    console.error(
+      `No price data was able to be fetched ${JSON.stringify(err.errors)}`,
+    );
+    return parseData(null, err, err.message);
+  }
 };
