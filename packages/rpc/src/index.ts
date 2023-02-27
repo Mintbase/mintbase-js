4bd931c9ecd01ec6c97cfa960bc3769fa76f8af5
cif <email@benipsen.com>
2023-02-27 08:21:54 -0700

    feat(rpc): :sparkles: Add accountExists method to rpc

--- packages/rpc/src/index.ts
+++ packages/rpc/src/index.ts
@@ -1,3 +1,5 @@
 export * from './methods/blockheight';
 export * from './methods/txnstatus';
 export * from './methods/balance';
+export * from './methods/payouts';
+export * from './methods/account';
