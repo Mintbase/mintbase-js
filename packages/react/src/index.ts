8d864d3319b7e956bd5ef13b2d5c06564313a4b5
cif <email@benipsen.com>
2023-03-02 11:20:01 -0700

    fix(react): :package: Update index export to include near price hook

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,3 +2,4 @@ export * from './WalletContext';
 // export * from './hooks/useMinter';
 export * from './hooks/useTokenById';
 export * from './hooks/useOwnedNftsByStores';
+export * from './hooks/useNearPrice';
