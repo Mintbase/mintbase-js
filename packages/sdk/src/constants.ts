f299daa98ca558ed01f827c3b5dfa06fcc1cab84
Ruben Marcus <ruben@rubenmarcus.dev>
2023-05-05 12:50:22 +0100

    usdc-fix

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -8,7 +8,7 @@ export const TOKEN_CONTRACT_SPEC = 'nft-1.0.0';
 // TODO: This should be reduced to 3.5 NEAR once the new factory/stores
 // are deployed to mainnet.
 export const CONTRACT_DEPOSIT = `6500000${'0'.repeat(18)}`; // 6.5 NEAR
-export const LISTING_DEPOSIT = `800${'0'.repeat(18)}`; // 0.8 milliNEAR
+export const LISTING_DEPOSIT = `1000${'0'.repeat(18)}`; // 1 milliNEAR
 
 export const GAS_CONSTANTS = {
   DEFAULT_GAS: '200000000000000',
