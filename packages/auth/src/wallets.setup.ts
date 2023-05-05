68824ed5952f05b345ba0f13f345e2ef150b49c2
Ruben Marcus <ruben@rubenmarcus.dev>
2023-05-05 13:10:48 +0100

    patch release

--- packages/auth/src/wallets.setup.ts
+++ packages/auth/src/wallets.setup.ts
@@ -1,7 +1,7 @@
 import { setupNearWallet } from '@near-wallet-selector/near-wallet';
 import { setupMeteorWallet } from '@near-wallet-selector/meteor-wallet';
 import { setupSender } from '@near-wallet-selector/sender';
-import { setupHereWallet } from '@near-wallet-selector/here-wallet';
+// import { setupHereWallet } from '@near-wallet-selector/here-wallet';
 import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
 
 import { WalletModuleFactory } from '@near-wallet-selector/core';
@@ -10,6 +10,5 @@ export const SUPPORTED_NEAR_WALLETS: Array<WalletModuleFactory> =[
   setupNearWallet(),
   setupMeteorWallet(),
   setupSender(),
-  setupHereWallet(),
   setupMyNearWallet(),
 ];
