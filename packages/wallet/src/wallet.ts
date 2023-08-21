e40efbe55d68f51daf23abeb6007996c4d56f5e1
Ruben Marcus <ruben@rubenmarcus.dev>
2023-08-21 15:46:48 +0100

    Fix/wallet refresh fix 2 (#381)

--- packages/wallet/src/wallet.ts
+++ packages/wallet/src/wallet.ts
@@ -174,12 +174,17 @@ export class MintbaseWallet {
 
       if (accountId) {
         this._initializeWalletState({ accountId });
+
+           return [{accountId: accountId, active:true}]
       }
-      return;
+   
     }
 
     if (urlParams?.accountId) {
       this._initializeWalletState({ accountId: urlParams?.accountId, publicKey: urlParams?.publicKey || '' });
+
+       return [{accountId:  urlParams?.accountId, publicKey: urlParams?.publicKey ,  active:true}]
+
     }
   }
 
@@ -195,6 +200,7 @@ export class MintbaseWallet {
     this._clearQueryParams();
 
     return [{publickKey: publicKey, accountId: accountId, active: true}]
+
   }
 
   private _setActiveAccountId(accountId: string) {
