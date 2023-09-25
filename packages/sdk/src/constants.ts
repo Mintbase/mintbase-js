66e6e7f5bbdea67447bcd03fc02c5fccdb195e40
Till <till.friesewinkel@googlemail.com>
2023-09-25 09:18:02 +0100

    Update comments

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -5,9 +5,7 @@ export const GAS = '200000000000000';
 export const MAX_GAS = '300000000000000';
 export const ONE_YOCTO = '1';
 export const TOKEN_CONTRACT_SPEC = 'nft-1.0.0';
-// TODO: This should be reduced to 3.5 NEAR once the new factory/stores
-// are deployed to mainnet.
-export const CONTRACT_DEPOSIT = `3500000${'0'.repeat(18)}`; // 6.5 NEAR
+export const CONTRACT_DEPOSIT = `3500000${'0'.repeat(18)}`; // 3.5 NEAR
 export const LISTING_DEPOSIT = `1000${'0'.repeat(18)}`; // 1 milliNEAR
 
 export const GAS_CONSTANTS = {
