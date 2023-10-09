53d83e7d8461dfaec221685988ce5e9adc976e9d
ruisantiago <ruisantiagomr@gmail.com>
2023-10-09 08:42:51 +0100

    Revert "redirect to homepage in mintbase wallet in consumer apps (#401)"
    This reverts commit 4fa770035ec56c4cb6a599d289ff99b5877176e1.

--- packages/wallet/src/wallet.ts
+++ packages/wallet/src/wallet.ts
@@ -53,7 +53,7 @@ export class MintbaseWallet {
 
   async signIn() {
     const currentUrl = new URL(window.location.href);
-    const newUrl = new URL(`${this.walletUrl}`);
+    const newUrl = new URL(`${this.walletUrl}/connect`);
     newUrl.searchParams.set('success_url', currentUrl.href);
     newUrl.searchParams.set('failure_url', currentUrl.href);
 
