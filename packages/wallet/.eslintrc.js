8f444d6895b3a0db3fb741b8a9d7cb7158b795c5
Ruben Marcus <ruben@rubenmarcus.dev>
2023-10-12 19:51:00 +0100

    tsconfig

---
+++ packages/wallet/.eslintrc.js
@@ -0,0 +1,7 @@
+const path = require('path');
+module.exports = {
+  "extends": "../../.eslintrc.json",
+  "parserOptions": {
+    "project": path.resolve(__dirname, "../../tsconfig.lint.json")
+  }
+}
