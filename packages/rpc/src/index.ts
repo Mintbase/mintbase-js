51c8b89848508ff8e0a69295b25716efaa387b38
Ruben Marcus <ruben@rubenmarcus.dev>
2023-10-16 16:16:16 +0100

    get blackHash

--- packages/rpc/src/index.ts
+++ packages/rpc/src/index.ts
@@ -5,3 +5,5 @@ export * from './methods/payouts';
 export * from './methods/account';
 export * from './methods/social';
 export * from './methods/getBlockHash';
+export * from './methods/getGasPrice';
+export * from './methods/getLatestGasPrice';
