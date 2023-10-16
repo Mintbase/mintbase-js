0ca688d94ffe77e95f83e183e9fe5b73ec43a7b6
Till <till.friesewinkel@googlemail.com>
2023-10-16 10:09:39 +0100

    Complete tests

--- packages/rpc/src/methods/keys.test.ts
+++ packages/rpc/src/methods/keys.test.ts
@@ -1,9 +1,11 @@
 import fetch from 'isomorphic-unfetch';
-import { getAccessKeys } from './keys';
+import { AccessKey, getAccessKeys } from './keys';
 
 jest.mock('isomorphic-unfetch');
 
 describe('keys', () => {
+  const getRes = async (): Promise<AccessKey[]> => await getAccessKeys('benipsen.near');
+
   it('should return access keys for accounts', async () => {
     (fetch as jest.Mock).mockResolvedValueOnce({
       json: jest.fn().mockResolvedValueOnce({
@@ -14,9 +16,29 @@ describe('keys', () => {
         },
       }),
     });
-    const res = await getAccessKeys('benipsen.near');
+    const res = await getRes();
     expect(res.length).toBe(1);
     expect(res[0].public_key).toBe('pubkey');
     expect(res[0].permission).toBe('FullAccess');
+  });
+
+  it('should throw on returned error', async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce({
+        result: { error: 'some error' },
+      }),
+    });
+
+    await expect(getRes).rejects.toThrow('some error');
+  });
+
+  it('should throw on malformed response', async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValueOnce({
+        result: { foo: 'bar' },
+      }),
+    });
+
+    await expect(getRes).rejects.toThrow('Malformed response');
   });
 });
