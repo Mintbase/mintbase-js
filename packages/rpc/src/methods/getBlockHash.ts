e6c05ca2edd3683d2e27279621771fe3a4023cb6
Ruben Marcus <ruben@rubenmarcus.dev>
2023-10-16 15:44:53 +0100

    get blackHash

--- packages/rpc/src/methods/getBlockHash.ts
+++ packages/rpc/src/methods/getBlockHash.ts
@@ -1,20 +1,15 @@
-import BN from 'bn.js';
 import { requestFromNearRpc } from '../util';
 
-export const getBlockHash = async (accountId: string): Promise<BN> => {
+export const getBlockHash = async (): Promise<number> => {
   const res = await requestFromNearRpc({
     jsonrpc: '2.0',
     id: 'dontcare',
-    method: 'query',
-    params: {
-      request_type: 'view_account',
-      finality: 'final',
-      account_id: accountId,
-    },
+    method: 'status',
+    params: [],
   });
-  const balanceString = res?.result?.block_hash;
-  if (!balanceString) {
+  const blockHeight = res?.result?.sync_info?.latest_block_hash;
+  if (!blockHeight) {
     throw new Error(`Malformed response: ${JSON.stringify(res)}`);
   }
-  return new BN(balanceString);
+  return blockHeight;
 };
