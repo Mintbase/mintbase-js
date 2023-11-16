01f0130a7b724ccf8cd5f16ef0dc281edc664726
Ruben Marcus <ruben@rubenmarcus.dev>
2023-11-16 11:23:47 +0000

    add support to contractId

--- packages/wallet/src/setup.ts
+++ packages/wallet/src/setup.ts
@@ -44,11 +44,7 @@ export function setupMintbaseWallet({
         walletUrl: resolveWalletUrl(moduleOptions.options.network.networkId, walletUrl),
       },
       init: (options) => {
-<<<<<<< HEAD
         return MintbaseWallet({ callback: callbackUrl, networkId: moduleOptions.options.network, successUrl, failureUrl, contractId,  ...options });
-=======
-        return MintbaseWallet({ callback: callbackUrl, networkId: moduleOptions.options.network, successUrl, failureUrl, ...options });
->>>>>>> beta
       },
     };
     return wallet;
