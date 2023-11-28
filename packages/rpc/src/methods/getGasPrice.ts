b694f80fc14a23e661082ec191d4c9b73cd0c90c
Till <31419678+tifrel@users.noreply.github.com>
2023-11-28 13:03:49 +0200

    Get gas price for current block height as default (#437)

--- packages/rpc/src/methods/getGasPrice.ts
+++ packages/rpc/src/methods/getGasPrice.ts
@@ -1,15 +1,15 @@
 import { requestFromNearRpc } from '../util';
 
-export const getGasPrice = async (hash: string): Promise<number> => {
+export const getGasPrice = async (hash?: string): Promise<number> => {
   const res = await requestFromNearRpc({
     jsonrpc: '2.0',
     id: 'dontcare',
     method: 'gas_price',
-    params: [hash],
+    params: [hash || null],
   });
-  const blockHeight = res?.result?.gas_price;
-  if (!blockHeight) {
+  const gasPrice = res?.result?.gas_price;
+  if (!gasPrice) {
     throw new Error(`Malformed response: ${JSON.stringify(res)}`);
   }
-  return blockHeight;
+  return gasPrice;
 };
