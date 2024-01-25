03c969834f4574a619f5be7dc3dab1ae4e33e3d8
Ruben Marcus <ruben@rubenmarcus.dev>
2024-01-25 11:41:55 +0000

    export wallet types

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,3 @@
 export * from './MintbaseWalletContext';
 export * from './hooks/useNearPrice';
+export { Wallet, WalletModuleFactory, WalletModule, WalletBehaviourFactory, BrowserWallet, Account } from '@near-wallet-selector/core';
