b34fa835a630cf22a3ea139fd5410c08d744db30
Till <till.friesewinkel@googlemail.com>
2024-02-07 14:14:36 +0000

    Reactivate previous tests

--- packages/sdk/src/createMetadata/createMetadata.test.ts
+++ packages/sdk/src/createMetadata/createMetadata.test.ts
@@ -38,127 +38,82 @@ describe('createMetadata method tests', () => {
   });
 
   // TODO: test new features
-  // FIXME: reactivate old tests
-  // test('createMetadata with floating point royalties and amount', () => {
-  //   const args = createMetadata({
-  //     contractAddress: contractAddress,
-  //     metadata: { reference, media },
-  //     ownerId: ownerId,
-  //     royalties: { test: 0.23, test1: 0.12654, test2: 0.04421 },
-  //     amount: 2,
-  //   });
+  test('createMetadata with floating point royalties and amount', () => {
+    const args = createMetadata({
+      contractAddress: contractAddress,
+      metadata: { reference, media },
+      royalties: { test: 0.23, test1: 0.12654, test2: 0.04421 },
+      price: 1,
+    });
 
-  //   expect(args).toEqual({
-  //     contractAddress: contractAddress,
-  //     methodName: TOKEN_METHOD_NAMES.CREATE_METADATA,
-  //     args: {
-  //       owner_id: ownerId,
-  //       metadata: {
-  //         reference: reference,
-  //         media: media,
-  //       },
-  //       num_to_mint: 2,
-  //       royalty_args: {
-  //         percentage: 4008,
-  //         split_between: {
-  //           test: 5739,
-  //           test1: 3158,
-  //           test2: 1103,
-  //         },
-  //       },
-  //     },
-  //     deposit: '15070000000000000000000',
-  //     gas: GAS,
-  //   });
-  // });
+    expect(args).toEqual({
+      contractAddress: contractAddress,
+      methodName: TOKEN_METHOD_NAMES.CREATE_METADATA,
+      args: {
+        metadata: {
+          reference: reference,
+          media: media,
+        },
+        price: `1${'0'.repeat(24)}`,
+        metadata_id: null,
+        royalty_args: {
+          percentage: 4008,
+          split_between: {
+            test: 5739,
+            test1: 3158,
+            test2: 1103,
+          },
+        },
+        minters_allowlist: null,
+        max_supply: null,
+        last_possible_mint: null,
+      },
+      deposit: '9870000000000000000000',
+      gas: GAS,
+    });
+  });
 
-  // test('mint with flaoting point royalties without amount or specifying tokenIds', () => {
-  //   const args = mint({
-  //     contractAddress: contractAddress,
-  //     metadata: { reference, media },
-  //     ownerId: ownerId,
-  //     royalties: { test: 0.23, test1: 0.12654, test2: 0.04421 },
-  //   });
+  test('createMetadata with too many royalties', () => {
+    expect(() => {
+      createMetadata({
+        contractAddress: contractAddress,
+        metadata: { reference, media },
+        royalties: { test: 0.3, test1: 0.25 },
+        price: 1,
+      });
+    }).toThrow(ERROR_MESSAGES.INVALID_ROYALTY_PERCENTAGE);
+  });
 
-  //   expect(args).toEqual({
-  //     contractAddress: contractAddress,
-  //     methodName: TOKEN_METHOD_NAMES.MINT,
-  //     args: {
-  //       owner_id: ownerId,
-  //       metadata: {
-  //         reference: reference,
-  //         media: media,
-  //       },
-  //       num_to_mint: 1,
-  //       royalty_args: {
-  //         percentage: 4008,
-  //         split_between: {
-  //           test: 5739,
-  //           test1: 3158,
-  //           test2: 1103,
-  //         },
-  //       },
-  //     },
-  //     deposit: '9870000000000000000000',
-  //     gas: GAS,
-  //   });
-  // });
+  test('createMetadata with no reference', () => {
+    expect(() => {
+      createMetadata({
+        contractAddress: contractAddress,
+        metadata: { media },
+        royalties: { test: 0.3, test1: 0.25 },
+        price: 1,
+      });
+    }).toThrow(ERROR_MESSAGES.NO_REFERENCE);
+  });
 
-  // test('mint with too many royalties', () => {
-  //   expect(() => {
-  //     mint({
-  //       contractAddress: contractAddress,
-  //       metadata: { reference, media },
-  //       ownerId: ownerId,
-  //       royalties: { test: 0.3, test1: 0.25 },
-  //     });
-  //   }).toThrow(ERROR_MESSAGES.INVALID_ROYALTY_PERCENTAGE);
-  // });
+  test('createMetadata with no media', () => {
+    expect(() => {
+      createMetadata({
+        contractAddress: contractAddress,
+        metadata: { reference },
+        royalties: { test: 0.3, test1: 0.25 },
+        price: 1,
+      });
+    }).toThrow(ERROR_MESSAGES.NO_MEDIA);
+  });
 
-  // test('mint with no reference', () => {
-  //   expect(() => {
-  //     mint({
-  //       contractAddress: contractAddress,
-  //       metadata: { media },
-  //       ownerId: ownerId,
-  //       royalties: { test: 0.3, test1: 0.25 },
-  //     });
-  //   }).toThrow(ERROR_MESSAGES.NO_REFERENCE);
-  // });
-
-  // test('mint with no media', () => {
-  //   expect(() => {
-  //     mint({
-  //       contractAddress: contractAddress,
-  //       metadata: { reference },
-  //       ownerId: ownerId,
-  //       royalties: { test: 0.3, test1: 0.25 },
-  //     });
-  //   }).toThrow(ERROR_MESSAGES.NO_MEDIA);
-  // });
-
-  // test('mint with invalid amount', () => {
-  //   expect(() => {
-  //     mint({
-  //       contractAddress: contractAddress,
-  //       metadata: { reference },
-  //       noMedia: true,
-  //       noReference: true,
-  //       ownerId: ownerId,
-  //       royalties: { test: 0.2, test1: 0.25 },
-  //       amount: -2,
-  //     });
-  //   }).toThrow(ERROR_MESSAGES.INVALID_AMOUNT);
-  // });
-
-  // test('mint with negative royalties', () => {
-  //   expect(() => {
-  //     mint({
-  //       contractAddress: contractAddress,
-  //       metadata: { reference, media },
-  //       ownerId: ownerId,
-  //       royalties: { test: -0.3, test1: 0.4 },
-  //     });
-  //   }).toThrow(ERROR_MESSAGES.NEGATIVE_ROYALTIES);
-  // });
+  test('createMetadata with negative royalties', () => {
+    expect(() => {
+      createMetadata({
+        contractAddress: contractAddress,
+        metadata: { reference, media },
+        royalties: { test: -0.3, test1: 0.4 },
+        price: 1,
+      });
+    }).toThrow(ERROR_MESSAGES.NEGATIVE_ROYALTIES);
+  });
 });
