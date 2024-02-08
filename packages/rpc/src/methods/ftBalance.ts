6ac1cfb2887a2a97aaef0c5a5accb1fb2edd0d87
Ruben Marcus <ruben@rubenmarcus.dev>
2024-02-08 16:57:56 +0000

    add network option rpc (#448)

--- packages/rpc/src/methods/ftBalance.ts
+++ packages/rpc/src/methods/ftBalance.ts
@@ -1,9 +1,10 @@
 import { callViewMethod } from '../util';
 
-export const ftBalance = async ({ contractId, accountId }): Promise<string> => {
+export const ftBalance = async ({ contractId, accountId, network }): Promise<string> => {
   return callViewMethod<string>({
     contractId,
     method: 'ft_balance_of',
     args: { account_id: accountId },
+    network: network,
   });
 };
