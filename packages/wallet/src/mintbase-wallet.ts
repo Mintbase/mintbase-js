72cc292fb576abdac2712c3a3d1eb914331eb104
Luís Freitas <microchipgnu@gmail.com>
2024-05-07 18:37:45 +0100

    revert

--- packages/wallet/src/mintbase-wallet.ts
+++ packages/wallet/src/mintbase-wallet.ts
@@ -3,12 +3,10 @@ import * as nearAPI from 'near-api-js';
 import type {
   Action,
   BrowserWallet,
-  FinalExecutionOutcome,
+  Transaction,
   WalletBehaviourFactory,
 } from '@near-wallet-selector/core';
 import { getCallbackUrl } from './utils';
-import { createAction } from '@near-wallet-selector/wallet-utils';
-
 
 export enum TransactionSuccessEnum {
   MINT = 'mint',
@@ -40,11 +38,15 @@ export type CallBackArgs = {
   type: TransactionSuccessEnum;
 }
 
+interface Networks {
+  mainnet: string;
+  testnet: string;
+}
 
 export const MintbaseWallet: WalletBehaviourFactory<
   BrowserWallet,
   {
-    networkId: string;
+    networkId: any;
     callback: string;
     successUrl?: string;
     failureUrl?: string;
@@ -60,282 +62,248 @@ export const MintbaseWallet: WalletBehaviourFactory<
   networkId,
 }) => {
 
-    const setupWalletState = async (): Promise<MintbaseWalletState> | null => {
-      if (typeof window !== undefined) {
-        const { connect, WalletConnection, keyStores } = nearAPI;
-        const connectionConfig = {
-          networkId: networkId,
-          keyStore: new keyStores.BrowserLocalStorageKeyStore(),
-          nodeUrl: options.network.nodeUrl,
-          walletUrl: metadata.walletUrl,
-          headers: {},
-        };
+  const setupWalletState = async (): Promise<MintbaseWalletState> | null => {
+    if (typeof window !== undefined) {
+      const { connect, WalletConnection, keyStores } = nearAPI;
 
-        const searchParams = new URL(window.location.href);
-        const acc = searchParams.searchParams.get('account_id');
-        //make near-api-js not throw without lak
-        if (acc && !contractId) {
-          localStorage.setItem(
-            'mintbase-wallet_wallet_auth_key',
-            JSON.stringify({
-              accountId: acc as string,
-              allKeys: [],
-            }),
-          );
-        }
+      const connectionConfig = {
+        networkId: networkId,
+        keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+        nodeUrl: options.network.nodeUrl,
+        walletUrl: metadata.walletUrl,
+        headers: {},
+      };
 
-        const nearConnection = await connect(connectionConfig);
-        const wallet = new WalletConnection(nearConnection, 'mintbase-wallet');
+      const searchParams = new URL(window.location.href);
+
+      const acc = searchParams.searchParams.get('account_id');
+
+      if (acc && acc?.length > 0) {
         localStorage.setItem('mintbase-wallet:callback_url', callback);
 
-        return {
-          wallet,
-        };
-      }
-
-      return null;
-    };
-
-    const state = await setupWalletState();
-
-    let activeAccountId: string;
-
-    const getAccountId = (): string => activeAccountId;
-
-    const isSignedIn = async (): Promise<boolean> => !!activeAccountId;
-
-    const signIn = async (): Promise<MintbaseWalletAccount[]> => {
-      const existingAccounts = await getAccounts();
-      const href = encodeURI(window?.location?.href);
-
-      if (existingAccounts.length) {
-        return existingAccounts;
-      }
-
-      await state.wallet.requestSignIn({
-        methodNames: [],
-        successUrl: successUrl || href,
-        failureUrl: failureUrl || href,
-        contractId: contractId,
-      });
-
-      return getAccounts();
-    };
-
-    const signOut = async (): Promise<void> => {
-      window.localStorage.removeItem('mintbase-wallet:account-data');
-
-      if (state.wallet.isSignedIn()) {
-        state.wallet.signOut();
-      }
-
-      return;
-    };
-
-
-    const assertValidSigner = (signerId: string): void => {
-      if (signerId && signerId !== state.wallet.getAccountId()) {
-        throw new Error(
-          `Cannot sign transactions for ${signerId} while signed in as ${activeAccountId}`,
+        localStorage.setItem(
+          'mintbase-wallet_wallet_auth_key',
+          JSON.stringify({
+            accountId: acc as string,
+            allKeys: [],
+          }),
         );
       }
-    };
 
-    const signAndSendTransactions = async ({ transactions, callbackUrl }): Promise<void> => {
-      if (!state.wallet.isSignedIn()) {
-        throw new Error('Wallet not signed in');
-      }
-      //// near-api-js code fails if no lak
-      // const { cbUrl } = getCallbackUrl(callbackUrl ?? '');
+      const nearConnection = await connect(connectionConfig);
 
-      // return state.wallet.requestSignTransactions({
-      //   transactions: await transformTransactions(transactions),
-      //   callbackUrl: cbUrl,
-      // });
+      const wallet = new WalletConnection(nearConnection, 'mintbase-wallet');
 
-      const { cbUrl } = getCallbackUrl(callbackUrl ?? '');
+      return {
+        wallet,
+      };
+    }
 
-      for (const { signerId } of transactions) {
-        assertValidSigner(signerId);
-      }
-      const stringifiedParam = JSON.stringify(transactions);
+    return null;
+  };
 
-      const urlParam = encodeURIComponent(stringifiedParam);
-      const newUrl = new URL(`${metadata.walletUrl}/sign-transaction`);
-      newUrl.searchParams.set('transactions_data', urlParam);
-      newUrl.searchParams.set('callback_url', cbUrl);
+  const state = await setupWalletState();
 
-      window.location.assign(newUrl.toString());
-      return;
-    };
+  let activeAccountId: string;
 
-    const signAndSendTransaction = async ({
-      receiverId,
-      actions,
-      signerId,
-      callbackUrl,
-    }: {
-      receiverId: string;
-      actions: Array<Action>;
-      signerId: string;
-      callbackUrl: string;
-    }): Promise<FinalExecutionOutcome> => {
+  const getAccountId = (): string => activeAccountId;
+
+  const isSignedIn = async (): Promise<boolean> => !!activeAccountId;
+
+  const signIn = async (): Promise<MintbaseWalletAccount[]> => {
+    const existingAccounts = await getAccounts();
+    const origin = encodeURI(window?.location?.origin);
+
+    if (existingAccounts.length) {
+      return existingAccounts;
+    }
+
+    await state.wallet.requestSignIn({
+      methodNames: [],
+      successUrl: origin,
+      failureUrl: origin,
+    });
+
+    return getAccounts();
+  };
+
+  const signOut = async (): Promise<void> => {
+    window.localStorage.removeItem('mintbase-wallet:account-data');
+
+    if (state.wallet.isSignedIn()) {
+      state.wallet.signOut();
+    }
+
+    return;
+  };
+
+  const assertValidSigner = (signerId: string): void => {
+    if (signerId && signerId !== state.wallet.getAccountId()) {
+      throw new Error(
+        `Cannot sign transactions for ${signerId} while signed in as ${activeAccountId}`,
+      );
+    }
+  };
+
+  const signAndSendTransactions = async ({
+    transactions,
+    callbackUrl,
+  }: {
+    transactions: Array<Transaction>;
+    callbackUrl?: string;
+  }): Promise<void> => {
+    const { cbUrl } = getCallbackUrl(callbackUrl ?? '');
+
+    // fix txn length issue
+    // if (transactions?.length > 10) {
+    // }
+
+    for (const { signerId } of transactions) {
       assertValidSigner(signerId);
+    }
+    const stringifiedParam = JSON.stringify(transactions);
 
-      if (!receiverId && !contractId) {
-        throw new Error('No receiver found to send the transaction to');
-      }
+    const urlParam = encodeURIComponent(stringifiedParam);
+    const newUrl = new URL(`${metadata.walletUrl}/sign-transaction`);
+    newUrl.searchParams.set('transactions_data', urlParam);
+    newUrl.searchParams.set('callback_url', cbUrl);
 
-      const { cbUrl } = getCallbackUrl(callbackUrl ?? '');
+    window.location.assign(newUrl.toString());
+    return;
+  };
 
-      const callback = cbUrl || successUrl;
+  const signAndSendTransaction =  async ({
+    receiverId,
+    actions,
+    signerId,
+    callbackUrl,
+  }: {
+    receiverId: string;
+    actions: Array<Action>;
+    signerId: string;
+    callbackUrl: string;
+  }): Promise<void> => {
+    assertValidSigner(signerId);
 
-      if (!contractId) {
-        const newUrl = new URL(`${metadata.walletUrl}/sign-transaction`);
-        const stringifiedParam = JSON.stringify([{ receiverId, signerId, actions }]);
-        const urlParam = encodeURIComponent(stringifiedParam);
-        newUrl.searchParams.set('transactions_data', urlParam);
-        newUrl.searchParams.set('callback_url', callback);
-        window.location.assign(newUrl.toString());
-      }
-      const account = state.wallet.account();
+    if (!receiverId && !contractId) {
+      throw new Error('No receiver found to send the transaction to');
+    }
 
-      return account.signAndSendTransaction({
-        receiverId: receiverId || contractId,
-        actions: actions.map((action) => createAction(action)) as any,
-        walletCallbackUrl: callback,
-      });
-    };
+    const stringifiedParam = JSON.stringify([{ receiverId, signerId, actions }]);
 
+    const { cbUrl } = getCallbackUrl(callbackUrl ?? '');
 
-    const verifyOwner = async (): Promise<void> => {
-      throw new Error(`The verifyOwner method is not supported by ${metadata.name}`);
-    };
+    const urlParam = encodeURIComponent(stringifiedParam);
 
-    const signMessage = async ({ message, nonce, recipient, callbackUrl }): Promise<void> => {
-      const { cbUrl } = getCallbackUrl(callbackUrl ?? '');
+    const currentUrl = new URL(window.location.href);
 
-      const newUrl = new URL(`${metadata.walletUrl}/sign-message`);
-      newUrl.searchParams.set('message', message);
-      newUrl.searchParams.set('nonce', nonce);
-      newUrl.searchParams.set('recipient', recipient);
-      newUrl.searchParams.set('callbackUrl', cbUrl);
-      window.location.assign(newUrl.toString());
-    };
+    const newUrl = new URL(`${metadata.walletUrl}/sign-transaction`);
+    newUrl.searchParams.set('transactions_data', urlParam);
 
-    const verifyMessage = async ({ accountId, publicKey, signature, message, nonce, recipient, callbackUrl }): Promise<boolean> => {
-
-      const newUrl = new URL(`${metadata.walletUrl}/api/verify-message`);
-      newUrl.searchParams.set('message', message);
-      newUrl.searchParams.set('accountId', accountId);
-      newUrl.searchParams.set('publicKey', publicKey);
-      newUrl.searchParams.set('signature', signature);
-      newUrl.searchParams.set('nonce', nonce);
-      newUrl.searchParams.set('recipient', recipient);
-      newUrl.searchParams.set('callbackUrl', callbackUrl);
-
-      try {
-        const response = await fetch(newUrl.toString())
-        const data = await response.json();
-
-        const { isValid } = data
-        return isValid
-      } catch (e) {
-        return false
+    if (successUrl) {
+      if (successUrl && successUrl.length > 0) {
+        newUrl.searchParams.set(
+          'success_url',
+          successUrl || currentUrl.toString(),
+        );
       }
     }
 
-    const getAvailableBalance = async (): Promise<void> => {
-      // const accountId = state.wallet.getAccountId();
-      // return await getBalance(accountId);
-      throw (`The getAvailableBalance method is not supported by ${metadata.name}`);
-    };
+    newUrl.searchParams.set('callback_url', cbUrl);
 
-    const getAccounts = async (): Promise<MintbaseWalletAccount[]> => {
-      const accountId = state.wallet.getAccountId();
-      const account = state.wallet.account();
-
-      if (!accountId || !account) {
-        return [];
-      }
-
-      const currentAccount: string = window.localStorage.getItem(
-        'mintbase-wallet:account-creation-data',
-      )!;
-
-      return [
-        {
-          accountId,
-          publicKey: JSON.parse(currentAccount)?.devicePublicKey,
-        },
-      ];
-    };
-
-    const switchAccount = async (id: string): Promise<null> => {
-      //TODO fix
-      setActiveAccountId(id);
-
-      return null;
-    };
-
-    const setActiveAccountId = (accountId: string): null => {
-      activeAccountId = accountId;
-      window.localStorage.setItem('mintbase-wallet:activeAccountId', accountId);
-
-      return null;
-    };
-
-    // const transformTransactions = async (
-    //   transactions: Array<Optional<Transaction, 'signerId'>>,
-    // ): Promise<Array<nearAPI.transactions.Transaction>> => {
-    //   const account = state.wallet.account();
-    //   const { networkId, signer, provider } = account.connection;
-
-    //   const localKey = await signer.getPublicKey(account.accountId, networkId);
-
-    //   return Promise.all(
-    //     transactions.map(async (transaction, index) => {
-    //       const actions = transaction.actions.map((action) =>
-    //         createAction(action),
-    //       );
-    //       const accessKey = await account.accessKeyForTransaction(
-    //         transaction.receiverId,
-    //         actions as any,
-    //         localKey,
-    //       );
-
-    //       if (!accessKey) {
-    //         throw new Error(
-    //           `Failed to find matching key for transaction sent to ${transaction.receiverId}`,
-    //         );
-    //       }
-
-    //       const block = await provider.block({ finality: 'final' });
-
-    //       return nearAPI.transactions.createTransaction(
-    //         account.accountId,
-    //         nearAPI.utils.PublicKey.from(accessKey.public_key),
-    //         transaction.receiverId,
-    //         accessKey.access_key.nonce + index + 1,
-    //         actions as any,
-    //         nearAPI.utils.serialize.base_decode(block.header.hash),
-    //       );
-    //     }),
-    //   );
-    // };
-
-    return {
-      getAccountId,
-      isSignedIn,
-      signIn,
-      signOut,
-      signAndSendTransaction,
-      verifyOwner,
-      signMessage,
-      getAvailableBalance,
-      getAccounts,
-      switchAccount,
-      signAndSendTransactions,
-      verifyMessage
-    };
+    window.location.assign(newUrl.toString());
+    return;
   };
+
+  const verifyOwner = async (): Promise<void> => {
+    throw new Error(`The verifyOwner method is not supported by ${metadata.name}`);
+  };
+
+  const signMessage = async ({ message, nonce, recipient, callbackUrl }): Promise<void> => {
+    const { cbUrl } = getCallbackUrl(callbackUrl ?? '');
+
+    const newUrl = new URL(`${metadata.walletUrl}/sign-message`);
+    newUrl.searchParams.set('message', message);
+    newUrl.searchParams.set('nonce', nonce);
+    newUrl.searchParams.set('recipient', recipient);
+    newUrl.searchParams.set('callbackUrl', cbUrl);
+    window.location.assign(newUrl.toString());
+  };
+
+  const verifyMessage = async ({ accountId, publicKey, signature, message, nonce, recipient, callbackUrl }): Promise<boolean> => {
+
+    const newUrl = new URL(`${metadata.walletUrl}/api/verify-message`);
+    newUrl.searchParams.set('message', message);
+    newUrl.searchParams.set('accountId', accountId);
+    newUrl.searchParams.set('publicKey', publicKey);
+    newUrl.searchParams.set('signature', signature);
+    newUrl.searchParams.set('nonce', nonce);
+    newUrl.searchParams.set('recipient', recipient);
+    newUrl.searchParams.set('callbackUrl', callbackUrl);
+
+    try {
+      const response = await fetch(newUrl.toString())
+      const data = await response.json();
+
+      const { isValid } = data
+      return isValid
+    } catch (e) {
+      return false
+    }
+  }
+
+  const getAvailableBalance = async (): Promise<void> => {
+    // const accountId = state.wallet.getAccountId();
+    // return await getBalance(accountId);
+    throw (`The getAvailableBalance method is not supported by ${metadata.name}`);
+  };
+
+  const getAccounts = async (): Promise<MintbaseWalletAccount[]> => {
+    const accountId = state.wallet.getAccountId();
+    const account = state.wallet.account();
+
+    if (!accountId || !account) {
+      return [];
+    }
+
+    const currentAccount: string = window.localStorage.getItem(
+      'mintbase-wallet:account-creation-data',
+    )!;
+
+    return [
+      {
+        accountId,
+        publicKey: JSON.parse(currentAccount)?.devicePublicKey,
+      },
+    ];
+  };
+
+  const switchAccount = async (id: string): Promise<null> => {
+    //TODO fix
+    setActiveAccountId(id);
+
+    return null;
+  };
+
+  const setActiveAccountId = (accountId: string): null => {
+    activeAccountId = accountId;
+    window.localStorage.setItem('mintbase-wallet:activeAccountId', accountId);
+
+    return null;
+  };
+
+  return {
+    getAccountId,
+    isSignedIn,
+    signIn,
+    signOut,
+    signAndSendTransaction,
+    verifyOwner,
+    signMessage,
+    getAvailableBalance,
+    getAccounts,
+    switchAccount,
+    signAndSendTransactions,
+    verifyMessage
+  };
+};
