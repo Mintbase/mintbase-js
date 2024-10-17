4d726c38f59dc15fa20e53332a2fbd4e46620894
Till <till.friesewinkel@googlemail.com>
2024-10-17 09:36:57 +0100

    Add unbridged stablecoins

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -87,13 +87,23 @@ export enum NEAR_BETA_RPC_ENDPOINTS  {
   testnet = 'https://beta.rpc.testnet.near.org',
 }
 
-export enum USDC_ADDRESS {
+export enum USDCe_ADDRESS {
   mainnet = 'a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.factory.bridge.near',
   testnet = 'usdc.fakes.testnet',
 }
 
-export enum USDT_ADDRESS {
+export enum USDTe_ADDRESS {
   mainnet = 'dac17f958d2ee523a2206206994597c13d831ec7.factory.bridge.near',
+  testnet = 'usdt.fakes.testnet',
+}
+
+export enum USDC_ADDRESS {
+  mainnet = '17208628f84f5d6ad33f0da3bbbeb27ffcb398eac501a31bd6ad2011e36133a1',
+  testnet = '3e2210e1184b45b64c8a434c0a7e7b23cc04ea7eb7a6c3c32520d03d4afcb8af',
+}
+
+export enum USDT_ADDRESS {
+  mainnet = 'usdt.tether-token.near',
   testnet = 'usdt.fakes.testnet',
 }
 
